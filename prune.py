c11dce3cf62f2733725b3b607e7b13c5a25008b8
Mahmoud Hussein <mahmoud.hussein@inf.ethz.ch>
2024-01-04 16:13:06 +0100

    changed import

--- prune.py
+++ prune.py
@@ -1,10 +1,9 @@
 import torch
-from torch import nn
-
+import torch.nn.utils.prune
 def prune_model(model,fraction=0.5):
     for name, module in model.named_modules():
         if isinstance(module,torch.nn.Conv2d):
-            nn.utils.prune.l1_unstructured(module,name='weight',amount=fraction)
+            torch.nn.utils.prune.l1_unstructured(module,name='weight',amount=fraction)
     print('---------let\'s see result after pruning-------------' )
     print(dict(model.named_buffers()).keys())
 
