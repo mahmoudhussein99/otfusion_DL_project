9ce0c1c61e72910b28a7bdafb5b5aac70271357f
Mahmoud Hussein <mahmoudmohamed_ashour@yahoo.com>
2024-01-05 20:14:57 +0100

    added whether to prune or not flag

--- main.py
+++ main.py
@@ -99,27 +99,28 @@ if __name__ == '__main__':
                 log_dict['test_losses'] = []
                 recheck_accuracies.append(routines.test(args, model, test_loader, log_dict))
             print("Rechecked accuracies are ", recheck_accuracies)
-        print('----------Prune the 2 Parent models now---------')
-        for model in models:
-            prune.prune_model(model, args.prune_frac, args.prune_type)
-
-        print('--------Rechecking accuracies again!--------')
-        if args.recheck_cifar or args.recheck_acc:
-            recheck_accuracies = []
-            i=0
-            epoch =0
+        if args.pruning:
+            print('----------Prune the 2 Parent models now---------')
             for model in models:
-                log_dict = {}
-                log_dict['test_losses'] = []
-                acc = routines.test(args, model, test_loader, log_dict)
-                print(f'----- Saving Pruned model{i}-------')
-                import os
-                output_root_dir = "{}/{}_models_ensembled/".format(args.baseroot, (args.dataset).lower())
-                output_root_dir = os.path.join(output_root_dir, args.exp_name, "pruned_parents")
-                cifar_train.store_checkpoint(output_root_dir,f'model_{i}.pruned.intial.checkpoint',model,epoch,acc)
-                recheck_accuracies.append(acc)
-                i += 1
-            print("Rechecked accuracies are ", recheck_accuracies)
+                prune.prune_model(model, args.prune_frac, args.prune_type)
+
+            print('--------Rechecking accuracies again!--------')
+            if args.recheck_cifar or args.recheck_acc:
+                recheck_accuracies = []
+                i=0
+                epoch =0
+                for model in models:
+                    log_dict = {}
+                    log_dict['test_losses'] = []
+                    acc = routines.test(args, model, test_loader, log_dict)
+                    print(f'----- Saving Pruned model{i}-------')
+                    import os
+                    output_root_dir = "{}/{}_models_ensembled/".format(args.baseroot, (args.dataset).lower())
+                    output_root_dir = os.path.join(output_root_dir, args.exp_name, "pruned_parents")
+                    cifar_train.store_checkpoint(output_root_dir,f'model_{i}.pruned.intial.checkpoint',model,epoch,acc)
+                    recheck_accuracies.append(acc)
+                    i += 1
+                print("Rechecked accuracies are ", recheck_accuracies)
 
         # print('checking named modules of model0 for use in compute_activations!', list(models[0].named_modules()))
 
--- parameters.py
+++ parameters.py
@@ -241,6 +241,8 @@ def get_parser():
     parser.add_argument('--dist-epochs', default=60, type=int, help='number of distillation epochs')
 
     parser.add_argument('--handle-skips', action='store_true', help='handle shortcut skips in resnet which decrease dimension')
+    parser.add_argument('--prune', action='store_true', help='use pruning in the load models')
+
     parser.add_argument('--prune-frac', default=0.5, type=float, help='fraction of l1-norm layerwise pruning (default: 0.5)')
     parser.add_argument('--prune-type', type=str, default='unstructured', choices=['unstructured', 'structured'], help='what type of pruning to use (default: unstructured)')
     parser.add_argument('--experiment-name', type=str, default='youre-an-idiot-that-didnt-name-the-experiment', help='name of the experiment')
