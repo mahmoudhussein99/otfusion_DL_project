0e7819144cbf99b04a364b086861de04b2fad59d
Geventh Vignendra <gvignen@student-cluster.inf.ethz.ch>
2024-01-08 11:30:06 +0100

    Functionality to plot from raw_dumps

---
+++ evaluate_hessian/plot_multiple_experiments.py
@@ -0,0 +1,30 @@
+import os
+import numpy as np
+import json
+
+from util_core import plot_loss_landscape, plot_ev_density
+
+basepath = './results' # DO NOT CHANGE
+experiments = [
+    'exp_cifar10_resnet18_nobias_nobn_no_pruning',
+]
+
+for exp in experiments:
+    path = os.path.join(basepath, exp)
+
+    print(f'EXPERIMENT: {exp[4:]}')
+    print('* Loading Raw Dumps', end = ' ')
+    
+    config = json.load(open(f'{path}/config.json'))
+    loss_landscape = json.load(open(f'{path}/raw_dumps/loss_landscape.json'))
+    eigenvalues = json.load(open(f'{path}/raw_dumps/eigenvalues.json'))
+    traces = json.load(open(f'{path}/raw_dumps/traces.json'))
+    eigenvalue_density = json.load(open(f'{path}/raw_dumps/eigenvalue_density.json'))
+    
+    print('* Plotting Experiment', end = ' ')
+
+    plot_path = os.path.join(path, "final_plots")
+    plot_loss_landscape(config, loss_landscape, plot_path)
+    plot_ev_density(eigenvalue_density, plot_path)
+
+    print('DONE!')
