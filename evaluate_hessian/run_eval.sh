f4f4a74f6984573cef3a68b8b8261b12abf6d6ff
Mahmoud Hussein <mahmoudmohamed_ashour@yahoo.com>
2024-01-08 20:36:33 +0100

    add cifar100 structured

--- evaluate_hessian/run_eval.sh
+++ evaluate_hessian/run_eval.sh
@@ -3,6 +3,6 @@
 #SBATCH --gpus=1
 #SBATCH --time=30:00:00
 #SBATCH --account=deep_learning
-#SBATCH --output=logs.analysis_cifar10_vgg11_experiments.out
+#SBATCH --output=logs.analysis_cifar100_vgg11_experiments.out
 
 python3 analyse_multiple_experiments.py 
